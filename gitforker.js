1ab09a85e57873b06f27b24614820dc2f557166a
ajaswa <ajaswa@gmail.com>
2015-03-02 14:58:58 -0700

    cleaning up all the little things.

--- gitforker.js
+++ gitforker.js
@@ -1,25 +1,25 @@
 /* gitforker
- * v0.0.1
- *
+ * v0.0.2
+ * Copyright 2013-2015 Andrew Jaswa
  * Adding "Fork me on GitHub" to your site without images.
  */
 
 (function () {
-  function getQueryStrings(url) { 
+  function getQueryStrings(url) {
     var assoc  = {},
         decode = function (s) { return decodeURIComponent(s.replace(/\+/g, " ")); },
         queryString = url.substring(url.indexOf("?")+1),
-        keyValues = queryString.split('&'); 
+        keyValues = queryString.split('&');
 
-    for(var i in keyValues) { 
+    for(var i in keyValues) {
       var key = keyValues[i].split('=');
       if (key.length > 1) {
         assoc[decode(key[0])] = decode(key[1]);
       }
-    } 
-
-    return assoc; 
+    }
+    return assoc;
   };
+
   function setOrientation(orientation) {
     var or = {};
     if (orientation === 'right') {
@@ -30,7 +30,7 @@
       or['p'] = 'left: -50px;';
     }
     return or;
-  }
+  };
 
   (function () {
     var qs = getQueryStrings(url);
@@ -43,46 +43,46 @@
         a = d.createElement('a'),
         s = d.createElement('span'),
         text = d.createTextNode('Fork me on GitHub'),
-        styleA = '',
-        styleB = '',
+        styleAnchor = '',
+        styleSpan = '',
         bg = qs['bg'] || '#121520',
         fg = qs['fg'] || '#eeeeee',
         or = setOrientation(qs['orientation'] || 'left');
 
-    styleA = "-webkit-transform: rotate("+or['r']+");"+
-              "-o-transform: rotate("+or['r']+");"+
-              "-moz-transform: rotate("+or['r']+");"+
-              "-ms-transform: rotate("+or['r']+");"+
-              "transform: rotate("+or['r']+");"+
-              "position: fixed;"+
-              "top: 50px;"+
-              or['p']+
-              "padding: 2px 0;"+
-              "display: block;"+
-              "background-color: "+bg+";"+
-              "text-decoration: none;"+
-              "color: "+fg+";"+
-              "font-family: Geneva, Tahoma, Verdana, sans-serif;"+
-              "font-size: 16px;"+
-              "-moz-box-shadow: 0px 0px 5px "+bg+";"+
-              "-webkit-box-shadow: 0px 0px 5px "+bg+";"+
-              "box-shadow: 0px 0px 5px "+bg+";",
-    styleS = "display: block;"+
-              "height: 30px;"+
-              "line-height: 30px;"+
-              "width: 240px;"+
-              "border-top: 1px dashed "+fg+";"+
-              "border-bottom: 1px dashed "+fg+";"+
-              "padding: 0 0px;"+
-              "text-align: center;";
+    styleAnchor = "-webkit-transform: rotate("+or['r']+");"+
+                  "-o-transform: rotate("+or['r']+");"+
+                  "-moz-transform: rotate("+or['r']+");"+
+                  "-ms-transform: rotate("+or['r']+");"+
+                  "transform: rotate("+or['r']+");"+
+                  "position: fixed;"+
+                  "top: 50px;"+
+                  or['p']+
+                  "padding: 2px 0;"+
+                  "display: block;"+
+                  "background-color: "+bg+";"+
+                  "text-decoration: none;"+
+                  "color: "+fg+";"+
+                  "font-family: Geneva, Tahoma, Verdana, sans-serif;"+
+                  "font-size: 16px;"+
+                  "-moz-box-shadow: 0px 0px 5px "+bg+";"+
+                  "-webkit-box-shadow: 0px 0px 5px "+bg+";"+
+                  "box-shadow: 0px 0px 5px "+bg+";";
 
-    s.setAttribute('style', styleS);
-    a.setAttribute('style', styleA);
+    styleSpan = "display: block;"+
+                "height: 30px;"+
+                "line-height: 30px;"+
+                "width: 240px;"+
+                "border-top: 1px dashed "+fg+";"+
+                "border-bottom: 1px dashed "+fg+";"+
+                "padding: 0 0px;"+
+                "text-align: center;";
+
+    a.setAttribute('style', styleAnchor);
     a.setAttribute('href', qs['url']);
+    s.setAttribute('style', styleSpan);
 
     s.appendChild(text);
     a.appendChild(s);
     body.appendChild(a);
   })();
-
-})()
+})();
