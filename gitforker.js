48fa1a2e504cff48fb07f34362ab8a4992c19f70
ajaswa <ajaswa@gmail.com>
2015-03-02 20:26:10 -0700

    Now with less bytes!

--- gitforker.js
+++ gitforker.js
@@ -1,88 +1,65 @@
 /* gitforker
- * v0.0.2
+ * v0.0.3
  * Copyright 2013-2015 Andrew Jaswa
  * Adding "Fork me on GitHub" to your site without images.
  */
 
 (function () {
-  function getQueryStrings(url) {
-    var assoc  = {},
-        decode = function (s) { return decodeURIComponent(s.replace(/\+/g, " ")); },
-        queryString = url.substring(url.indexOf("?")+1),
-        keyValues = queryString.split('&');
-
-    for(var i in keyValues) {
-      var key = keyValues[i].split('=');
-      if (key.length > 1) {
-        assoc[decode(key[0])] = decode(key[1]);
-      }
-    }
-    return assoc;
-  };
+  function getParameterByName(url, name) {
+    var value = url.match(new RegExp("[\?\&]" + name + "=([^\&]*)(\&?)","i"));
+    return value ? value[1] : value;
+  }
 
   function setOrientation(orientation) {
-    var or = {};
+    var or = {'r': '-45deg', 'p': 'left:-50px;'};
     if (orientation === 'right') {
       or['r'] = '45deg';
-      or['p'] = 'right: -50px;';
-    } else {
-      or['r'] = '-45deg';
-      or['p'] = 'left: -50px;';
+      or['p'] = 'right:-50px;';
     }
     return or;
   };
 
   (function () {
     var d = document,
-        url = d.getElementById('gitforker').getAttribute('src'),
-        qs = getQueryStrings(url);
-    if (!qs['url']) {
-      return;
-    }
-    var body = d.getElementsByTagName('body')[0],
+        paramUrl = d.getElementById('gitforker').getAttribute('src'),
         a = d.createElement('a'),
         s = d.createElement('span'),
-        text = d.createTextNode('Fork me on GitHub'),
-        styleAnchor = '',
-        styleSpan = '',
-        bg = qs['bg'] || '#121520',
-        fg = qs['fg'] || '#eeeeee',
-        or = setOrientation(qs['orientation'] || 'left');
+        text = d.createTextNode(getParameterByName(paramUrl, 'text') || 'Fork me on GitHub'),
+        url = getParameterByName(paramUrl, 'url'),
+        bg = getParameterByName(paramUrl, 'bg') || '#121212',
+        fg = getParameterByName(paramUrl, 'fg') || '#eee',
+        or = setOrientation(getParameterByName(url, 'orientation') || 'left'),
+        styleAnchor = "-webkit-transform:rotate("+or['r']+");"+
+                      "transform:rotate("+or['r']+");"+
+                      "position:fixed;"+
+                      "top:50px;"+
+                      or['p']+
+                      "padding: 2px 0;"+
+                      "display:block;"+
+                      "background:"+bg+";"+
+                      "text-decoration:none;"+
+                      "color:"+fg+";"+
+                      "font-family:Geneva,sans-serif;"+
+                      "font-size:16px;"+
+                      "box-shadow:0px 0px 5px "+bg+";",
+        styleSpan = "display:block;"+
+                    "height:30px;"+
+                    "line-height:30px;"+
+                    "width:240px;"+
+                    "border:1px dashed "+fg+";"+
+                    "border-left:0;"+
+                    "border-right:0;"+
+                    "text-align:center;";
 
-    styleAnchor = "-webkit-transform: rotate("+or['r']+");"+
-                  "-o-transform: rotate("+or['r']+");"+
-                  "-moz-transform: rotate("+or['r']+");"+
-                  "-ms-transform: rotate("+or['r']+");"+
-                  "transform: rotate("+or['r']+");"+
-                  "position: fixed;"+
-                  "top: 50px;"+
-                  or['p']+
-                  "padding: 2px 0;"+
-                  "display: block;"+
-                  "background-color: "+bg+";"+
-                  "text-decoration: none;"+
-                  "color: "+fg+";"+
-                  "font-family: Geneva, Tahoma, Verdana, sans-serif;"+
-                  "font-size: 16px;"+
-                  "-moz-box-shadow: 0px 0px 5px "+bg+";"+
-                  "-webkit-box-shadow: 0px 0px 5px "+bg+";"+
-                  "box-shadow: 0px 0px 5px "+bg+";";
-
-    styleSpan = "display: block;"+
-                "height: 30px;"+
-                "line-height: 30px;"+
-                "width: 240px;"+
-                "border-top: 1px dashed "+fg+";"+
-                "border-bottom: 1px dashed "+fg+";"+
-                "padding: 0 0px;"+
-                "text-align: center;";
-
+    if (!url) {
+      return;
+    }
     a.setAttribute('style', styleAnchor);
-    a.setAttribute('href', qs['url']);
+    a.setAttribute('href', url);
     s.setAttribute('style', styleSpan);
 
     s.appendChild(text);
     a.appendChild(s);
-    body.appendChild(a);
+    d.body.appendChild(a);
   })();
 })();
