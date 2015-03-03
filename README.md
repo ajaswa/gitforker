afab9e2e7e52eee6c16dc46bafbea002d790edff
ajaswa <ajaswa@gmail.com>
2015-03-03 09:12:39 -0700

    remove the setOrientation function.

--- README.md
+++ README.md
@@ -10,6 +10,7 @@ Using images for "Fork me on GitHub" is lame.
     <script id='gitforker' src='gitforker.min.js' data-url='https://github.com/ajaswa/gitforker'></script>
 
 ### Data Options
+Set via data attributes on the script tag.
 
   - `url` -
     Full url of your GitHub Repo.
@@ -32,7 +33,7 @@ Using images for "Fork me on GitHub" is lame.
 
 I always thought that using images for some things was kinda silly. The "Fork me on GitHub" ribbon seemed like a great canididate for using pure CSS with a lighter footprint.
 
-Grabing the "black" ribbon from the [GitHub ribbons page](https://github.com/blog/273-github-ribbons), the image is 7,327 bytes (when it's run though [ImageOptim](https://imageoptim.com/) it's 4,665 bytes (compress all the images!)). By comparison gitforker.js uncompressed is 1,800 bytes, over 2.7kb smaller. Compressed it is 851 bytes! Over 3.7kb smaller than the compressed image! And there is room for improvement in file size.
+Grabing the "black" ribbon from the [GitHub ribbons page](https://github.com/blog/273-github-ribbons), the image is 7,327 bytes (when it's run though [ImageOptim](https://imageoptim.com/) it's 4,665 bytes (compress all the images!)). By comparison gitforker.js uncompressed is 1,517 bytes, over 2.7kb smaller. Compressed it is 774 bytes! Over 3.7kb smaller than the compressed image! And there is room for improvement in file size.
 
 The one big draw back to GitForker is that the font used isn't the same as the GitHub Ribbons, they use [Collegiate](http://www.fontriver.com/font/collegiate/) and GitForker uses `font-family: Geneva, sans-serif;`. This also means you can easilly customize GitForker to suit your project. Including the Collegiate font for a few words would not be good for the payload.
 
--- gitforker.js
+++ gitforker.js
@@ -5,53 +5,43 @@
  */
 
 (function () {
-  function setOrientation(orientation) {
-    var or = {'r': '-45deg', 'p': 'left:-50px;'};
-    if (orientation === 'right') {
-      or['r'] = '45deg';
-      or['p'] = 'right:-50px;';
-    }
-    return or;
-  };
+  var d = document,
+      ds = d.querySelector('#gitforker').dataset,
+      a = d.createElement('a'),
+      s = d.createElement('span'),
+      bg = ds.bg || '#121212',
+      fg = ds.fg || '#eee',
+      or = {r: '-45deg', p: 'left:-50px;'};
+  if (ds.or === 'right') {
+    or = {r: '45deg', p: 'right:-50px;'};
+  }
+  var styleAnchor = "-webkit-transform:rotate("+or.r+");"+
+                    "transform:rotate("+or.r+");"+
+                    "position:fixed;"+
+                    "top:50px;"+
+                    or.p+
+                    "padding: 2px 0;"+
+                    "display:block;"+
+                    "background:"+bg+";"+
+                    "text-decoration:none;"+
+                    "color:"+fg+";"+
+                    "font: 16px/1.4 Geneva,sans-serif;"+
+                    "box-shadow:0px 0px 5px "+bg+";",
+      styleSpan = "display:block;"+
+                  "height:30px;"+
+                  "line-height:30px;"+
+                  "width:240px;"+
+                  "border:1px dashed "+fg+";"+
+                  "border-left:0;"+
+                  "border-right:0;"+
+                  "text-align:center;";
 
-  (function () {
-    var d = document,
-        ds = d.querySelector('#gitforker').dataset,
-        a = d.createElement('a'),
-        s = d.createElement('span'),
-        text = d.createTextNode(ds.text || 'Fork me on GitHub'),
-        bg = ds.bg || '#121212',
-        fg = ds.fg || '#eee',
-        or = setOrientation(ds.or || 'left'),
-        styleAnchor = "-webkit-transform:rotate("+or['r']+");"+
-                      "transform:rotate("+or['r']+");"+
-                      "position:fixed;"+
-                      "top:50px;"+
-                      or['p']+
-                      "padding: 2px 0;"+
-                      "display:block;"+
-                      "background:"+bg+";"+
-                      "text-decoration:none;"+
-                      "color:"+fg+";"+
-                      "font: 16px/1.4 Geneva,sans-serif;"+
-                      "box-shadow:0px 0px 5px "+bg+";",
-        styleSpan = "display:block;"+
-                    "height:30px;"+
-                    "line-height:30px;"+
-                    "width:240px;"+
-                    "border:1px dashed "+fg+";"+
-                    "border-left:0;"+
-                    "border-right:0;"+
-                    "text-align:center;";
-
-    if (ds.url) {
-      a.setAttribute('style', styleAnchor);
-      a.setAttribute('href', ds.url);
-      s.setAttribute('style', styleSpan);
-
-      s.appendChild(text);
-      a.appendChild(s);
-      d.body.appendChild(a);
-    }
-  })();
+  if (ds.url) {
+    a.setAttribute('style', styleAnchor);
+    a.setAttribute('href', ds.url);
+    s.setAttribute('style', styleSpan)
+    s.innerHTML = ds.text || 'Fork me on GitHub';
+    a.appendChild(s);
+    d.body.appendChild(a);
+  }
 })();
