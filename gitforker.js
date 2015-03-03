266a033834a526089457d142edf0b702f0d39374
ajaswa <ajaswa@gmail.com>
2015-03-03 11:17:02 -0700

    more clean up, down to 733 bytes.

--- gitforker.js
+++ gitforker.js
@@ -1,5 +1,5 @@
 /* gitforker
- * v0.0.5
+ * v0.0.6
  * Copyright 2013-2015 Andrew Jaswa
  * Adding "Fork me on GitHub" to your site without images.
  */
@@ -7,38 +7,36 @@
 (function () {
   var d = document,
       ds = d.querySelector('#gitforker').dataset,
-      a = d.createElement('a'),
-      s = d.createElement('span'),
       bg = ds.bg || '#121212',
       fg = ds.fg || '#eee',
-      or = {r: '-45deg', p: 'left:-50px;'};
-  if (ds.or === 'right') {
-    or = {r: '45deg', p: 'right:-50px;'};
-  }
-  var styleAnchor = "-webkit-transform:rotate("+or.r+");"+
-                    "transform:rotate("+or.r+");"+
+      r = ds.or === 'right' ? '45deg' : '-45deg',
+      p = ds.or || 'left',
+      a = d.createElement('a'),
+      s = d.createElement('span'),
+      styleAnchor = p+':-50px;'+
+                    "-webkit-transform:rotate("+r+");"+
+                    "transform:rotate("+r+");"+
+                    "background:"+bg+";"+
+                    "color:"+fg+";"+
+                    "box-shadow:0px 0px 5px "+bg+";"+
                     "position:fixed;"+
                     "top:50px;"+
-                    or.p+
+                    "text-decoration:none;"+
                     "padding: 2px 0;"+
                     "display:block;"+
-                    "background:"+bg+";"+
-                    "text-decoration:none;"+
-                    "color:"+fg+";"+
-                    "font: 16px/1.4 Geneva,sans-serif;"+
-                    "box-shadow:0px 0px 5px "+bg+";",
-      styleSpan = "display:block;"+
+                    "font: 16px Geneva,sans-serif;",
+      styleSpan = "border:1px dashed "+fg+";"+
+                  "display:block;"+
                   "height:30px;"+
                   "line-height:30px;"+
                   "width:240px;"+
-                  "border:1px dashed "+fg+";"+
                   "border-left:0;"+
                   "border-right:0;"+
                   "text-align:center;";
 
   if (ds.url) {
-    a.setAttribute('style', styleAnchor);
     a.setAttribute('href', ds.url);
+    a.setAttribute('style', styleAnchor);
     s.setAttribute('style', styleSpan)
     s.innerHTML = ds.text || 'Fork me on GitHub';
     a.appendChild(s);
